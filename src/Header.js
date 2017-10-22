7ff71d995e6535c5139eab3a2bb16c034560da93
Tyler Buchea <tyler@buchea.com>
2017-10-22 13:45:22 -0700

    Add latest projects to portfolio

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,9 @@
-import React, {
-  Component,
-} from 'react';
+import React, { Component } from 'react';
 
 export default class Header extends Component {
+  static defaultProps = {};
 
-  static defaultProps = {}
-
-  static propTypes = {}
+  static propTypes = {};
 
   constructor(props) {
     super(props);
@@ -17,7 +14,10 @@ export default class Header extends Component {
     return (
       <div className="container-restricted">
         <header className="header row">
-          <h1><a href="http://tylerbuchea.com">Tyler Buchea</a> <div>[ boo • shay ]</div></h1>
+          <h1>
+            <a href="http://tylerbuchea.com">Tyler Buchea</a>{' '}
+            <div>[ boo • shay ]</div>
+          </h1>
           <h2>Software Engineer</h2>
 
           <hr className="divider" />
@@ -25,55 +25,77 @@ export default class Header extends Component {
           <nav className="navbar">
             <div className="container">
               <ul className="navbar-list">
-
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="http://blog.tylerbuchea.com">
-                    <i className="fa fa-pencil"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="http://blog.tylerbuchea.com"
+                  >
+                    <i className="fa fa-pencil" />
                   </a>
                 </li>
 
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="https://stackoverflow.com/jobs/cv/employer/321320">
-                    <i className="fa fa-stack-overflow"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="https://stackoverflow.com/jobs/cv/employer/321320"
+                  >
+                    <i className="fa fa-stack-overflow" />
                   </a>
                 </li>
 
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="https://github.com/tylerbuchea">
-                    <i className="fa fa-github"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="https://github.com/tylerbuchea"
+                  >
+                    <i className="fa fa-github" />
                   </a>
                 </li>
 
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="https://angel.co/tyler-buchea">
-                    <i className="fa fa-angellist"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="https://angel.co/tyler-buchea"
+                  >
+                    <i className="fa fa-angellist" />
                   </a>
                 </li>
 
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="https://twitter.com/tylerbuchea">
-                    <i className="fa fa-twitter"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="https://twitter.com/tylerbuchea"
+                  >
+                    <i className="fa fa-twitter" />
                   </a>
                 </li>
 
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="https://www.linkedin.com/in/tyler-buchea-b3a43376">
-                    <i className="fa fa-linkedin"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="https://www.linkedin.com/in/tyler-buchea-b3a43376"
+                  >
+                    <i className="fa fa-linkedin" />
                   </a>
                 </li>
 
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="https://itunes.apple.com/us/developer/tyler-buchea/id1039566131">
-                    <i className="fa fa-apple"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="https://itunes.apple.com/us/developer/tyler-buchea/id1039566131"
+                  >
+                    <i className="fa fa-apple" />
                   </a>
                 </li>
 
                 <li className="navbar-item">
-                  <a className="icon navbar-link" href="https://play.google.com/store/apps/developer?id=Tyler+Buchea+LLC">
-                    <i className="fa fa-android"></i>
+                  <a
+                    className="icon navbar-link"
+                    href="https://play.google.com/store/apps/developer?id=Tyler+Buchea+LLC"
+                  >
+                    <i className="fa fa-android" />
                   </a>
                 </li>
-
               </ul>
             </div>
           </nav>
@@ -81,27 +103,71 @@ export default class Header extends Component {
 
         <section className="content row" role="main">
           <article className="content-item content-contact">
-
             <h2 className="title is-2">Let's Chat!</h2>
             <ul className="contact-list">
               <li>
-                <i className="fa fa-calendar"></i> <a href="https://calendly.com/tylerbuchea" onClick={() => window.ga('send', 'event', 'Contact Request', 'Calendly', 'Tried to schedule a calendly meeting')}>Schedule a meeting</a>
+                <i className="fa fa-calendar" />{' '}
+                <a
+                  href="https://calendly.com/tylerbuchea"
+                  onClick={() =>
+                    window.ga(
+                      'send',
+                      'event',
+                      'Contact Request',
+                      'Calendly',
+                      'Tried to schedule a calendly meeting'
+                    )}
+                >
+                  Schedule a meeting
+                </a>
               </li>
               <li>
-                <i className="fa fa-twitter"></i> <a href="https://twitter.com/tylerbuchea" target="_blank" onClick={() => window.ga('send', 'event', 'Contact Request', 'Twitter', 'Tried to contact me via Twitter')}>Tweet at me</a>
+                <i className="fa fa-twitter" />{' '}
+                <a
+                  href="https://twitter.com/tylerbuchea"
+                  target="_blank"
+                  onClick={() =>
+                    window.ga(
+                      'send',
+                      'event',
+                      'Contact Request',
+                      'Twitter',
+                      'Tried to contact me via Twitter'
+                    )}
+                >
+                  Tweet at me
+                </a>
               </li>
               <li>
-                <i className="fa fa-envelope"></i> <a href="mailto:tyler@buchea.com" target="_blank" onClick={() => window.ga('send', 'event', 'Contact Request', 'Email', 'Tried to contact me via email')}>Send me an email</a>
+                <i className="fa fa-envelope" />{' '}
+                <a
+                  href="mailto:tyler@buchea.com"
+                  target="_blank"
+                  onClick={() =>
+                    window.ga(
+                      'send',
+                      'event',
+                      'Contact Request',
+                      'Email',
+                      'Tried to contact me via email'
+                    )}
+                >
+                  Send me an email
+                </a>
               </li>
               <li>
                 <form action="//formspree.io/tyler@buchea.com" method="POST">
-                  <input type="email" name="_replyto" placeholder="your-email@example.com" style={{ fontSize: 12.5 }} />
+                  <input
+                    type="email"
+                    name="_replyto"
+                    placeholder="your-email@example.com"
+                    style={{ fontSize: 12.5 }}
+                  />
                   &nbsp;
                   <input type="submit" value="SEND" />
                 </form>
               </li>
             </ul>
-
           </article>
 
           <article className="content-item content-direct">
@@ -109,19 +175,22 @@ export default class Header extends Component {
               <div className="about-copy">
                 <h2 className="title is-2">Howdy,</h2>
                 <p>
-                  I'm Tyler Buchea, I'm a full stack JavaScript developer working
-                  and living in the Portland area. I like React, React Native,
-                  Redux, Express, NodeJS, GraphQL, and Relay. Add me on Snapchat: <strong>tylerbuchea</strong>.
+                  I'm Tyler Buchea, I'm a full stack JavaScript developer
+                  working and living in the Portland area. I like React, React
+                  Native, Redux, Express, NodeJS, Docker, and Kubernetes. Add me
+                  on Snapchat: <strong>tylerbuchea</strong>.
                 </p>
               </div>
-              <img src="img/icon-avatar.jpg" className="icon-avatar" role="presentation" />
+              <img
+                src="img/icon-avatar.jpg"
+                className="icon-avatar"
+                role="presentation"
+              />
             </div>
             <hr />
           </article>
-
         </section>
       </div>
     );
   }
-
 }
