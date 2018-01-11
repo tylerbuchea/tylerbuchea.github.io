e609935e8d705ff8bdea3b0511fc2c55989220e1
Tyler Buchea <tyler@buchea.com>
2018-01-10 23:01:15 -0800

    Update year

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, {
-  Component,
-} from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './index.css';
 import './skeleton.css';
@@ -10,7 +8,6 @@ import Header from './Header';
 import Modal from './Modal';
 
 export default class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -19,51 +16,46 @@ export default class App extends Component {
     };
   }
 
-  closeModal = () => this.setState({ modal: null })
-  openModal = (card) => this.setState({ modal: card })
+  closeModal = () => this.setState({ modal: null });
+  openModal = card => this.setState({ modal: card });
 
-  renderCards = (cards) =>
+  renderCards = cards => (
     <section className="section">
       <h2 className="title is-2 has-text-centered">Portfolio</h2>
       <div className="container">
         <div className="columns is-multiline">
-          {cards.map((card, index) =>
+          {cards.map((card, index) => (
             <div
               className="column is-one-third-tablet is-one-quarter-desktop"
               key={index}
             >
-              <Card
-                {...card}
-                openModal={() => this.openModal(card)}
-              />
+              <Card {...card} openModal={() => this.openModal(card)} />
             </div>
-          )}
+          ))}
         </div>
       </div>
     </section>
+  );
 
-  renderFooter = () =>
+  renderFooter = () => (
     <div className="container-restricted" style={{ paddingBottom: 30 }}>
       <div className="has-text-centered">
         <hr />
-        ©2017 Tyler Buchea. All Rights Reserved.
+        ©2018 Tyler Buchea. All Rights Reserved.
       </div>
     </div>
+  );
 
   render() {
     const cards = this.renderCards(this.state.cards);
     const footer = this.renderFooter();
     return (
       <div>
-        <Modal
-          close={this.closeModal}
-          modal={this.state.modal}
-        />
+        <Modal close={this.closeModal} modal={this.state.modal} />
         <Header />
         {cards}
         {footer}
       </div>
     );
   }
-
 }
